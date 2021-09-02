2e6c53df6f2c77afe469598ea381f58d453ef96b
Ajiharan <baskaranajiharan1243@gmail.com>
2021-09-02 17:53:09 +0530

    online status updated

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,12 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
+import { fromEvent, Observable, Subscription } from 'rxjs';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
+  onlineEvent: Observable<Event> | undefined;
+  offlineEvent: Observable<Event> | undefined;
+  subscription: Subscription = new Subscription();
+  isOnline: boolean = true;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.onlineEvent = fromEvent(window, 'online');
+    this.offlineEvent = fromEvent(window, 'offline');
+    this.subscription.add(
+      this.onlineEvent.subscribe((event) => {
+        console.log('online');
+        this.isOnline = true;
+      })
+    );
+
+    this.subscription.add(
+      this.offlineEvent.subscribe((event) => {
+        console.log('offline');
+        this.isOnline = false;
+      })
+    );
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
