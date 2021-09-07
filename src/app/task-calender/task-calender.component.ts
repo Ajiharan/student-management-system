c789e9e0038d71bf2cb5f043c333c23e661caddc
Ajiharan <baskaranajiharan1243@gmail.com>
2021-09-07 23:36:04 +0530

    calender part updated

--- src/app/task-calender/task-calender.component.ts
+++ src/app/task-calender/task-calender.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap/datepicker/ngb-date-struct';
 
 @Component({
   selector: 'app-task-calender',
   templateUrl: './task-calender.component.html',
-  styleUrls: ['./task-calender.component.scss']
+  styleUrls: ['./task-calender.component.scss'],
 })
 export class TaskCalenderComponent implements OnInit {
+  model: NgbDateStruct | undefined;
+  date: { year: number; month: number } | undefined;
+  constructor(private calendar: NgbCalendar) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
